5e19eb5b8c3f4c6d23def80643925cec9ce56559
sumit singh <summmmit44@gmail.com>
2014-10-30 22:00:32 +0900

    migrations generator added

---
+++ app/database/migrations/2014_10_30_125751_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('email', 50);
+			$table->string('username', 20);
+			$table->string('password', 60);
+			$table->string('password_temp', 60);
+			$table->string('code', 60);
+			$table->integer('active');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
