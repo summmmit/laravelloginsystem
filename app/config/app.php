4f290f560375bac3006358d4f176b220b8e67991
sumit singh <summmmit44@gmail.com>
2014-10-30 22:08:38 +0900

    seeders generators also added

--- app/config/app.php
+++ app/config/app.php
@@ -123,6 +123,7 @@ return array(
 		'Illuminate\Workbench\WorkbenchServiceProvider',
         'Way\Generators\GeneratorsServiceProvider',
         'Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider',
+        'Orangehill\Iseed\IseedServiceProvider',
 
 	),
 
